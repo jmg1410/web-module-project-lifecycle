8c09e074ad47aa2c70e742605f6c35d1e197c323
Jay Marmol <marmoljay@hotmail.com>
2024-11-08 19:00:09 -0500

    finished project

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+    <>
+    <form id='todoForm' 
+    onSubmit={this.props.onTodoFormSubmit}>
+        <input value={this.props.todoNameInput}
+         onChange={this.props.onTodoNameInputChange} 
+         type='text' placeholder='type to do'></input>
+        <input type='submit'></input>
+      </form>
+      <button onClick={this.props.toggleDisplayCompleted}>
+        {this.props.displayCompleteds ? 'Hide' : 'Show'} Completed
+        </button>
+    </>
+    )
   }
 }
